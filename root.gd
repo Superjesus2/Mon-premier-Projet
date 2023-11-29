ee41369e5bd9496b53e77c3958ab2bbee8c158ad
Super Jesus <john.l3mon@gmail.com>
2023-11-29 16:36:42 +0100

    PONG DAY TWO
    ugggh

--- root.gd
+++ root.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-
 #var elapsedtime = 0.
 #var coucouprinted = false
 #var frame = 0
 #var which_color = 0
 #var colors = [Color.RED, Color.BLUE, Color.GREEN, Color.NAVAJO_WHITE, Color.BLUE_VIOLET]
 
-	#Variables du Level
+	# Variables du Level
 var width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var height = ProjectSettings.get_setting("display/window/size/viewport_height")
 var edgeleft = (width/100)*2
@@ -15,79 +14,96 @@ var edgeright = width-edgeleft
 var edgetop = (height/100)*2
 var edgebottom = height-edgetop
 
-	#Variables des Players
-@onready var playerone = $playerone
-@onready var playertwo = $playertwo
-	#Variables player One
-@onready var playerone_size_y = playerone.size.y
-@onready var playerone_size_x = playerone.size.x
-	#Variables player Two
-@onready var playertwo_size_y = playerone_size_y
-@onready var playertwo_size_x = playerone_size_x
-
-
-var SPEED = 800  #pixels par seconde
+	# VARIABLES DE LA BALLE
+var SPEEDMAX = 600  #pixels par seconde
 var speedx = 0
 var speedy = 0
 
 
+	# Variables des Players
+@onready var playerone = $playerone
+@onready var playertwo = $playertwo
+var speed1 = 0
+var speed2 = 0
+var playerSPEEDMAX = 6
 
-	
+	# Variables player One
+@onready var playerone_size_y = playerone.size.y
+@onready var playerone_size_x = playerone.size.x
+var playerone_cango_up = true
+var playerone_cango_down = true
+
+	# Variables player Two
+@onready var playertwo_size_y = playerone_size_y
+@onready var playertwo_size_x = playerone_size_x
+var playertwo_cango_up = true
+var playertwo_cango_down = true
+
+
+
+
+
+
 
 
 func _ready():
-	
-	
+
 		#VARIABLES DES PLAYERS
-	var players_edgeleft = edgeleft + playerone_size_x
-	var players_edgeright = edgeright - playerone_size_x
-	var players_edgetop = edgetop + (playerone_size_y / 2)
-	var players_edgebottom = edgebottom - (playerone_size_y / 2)
-	
-	print(players_edgeleft)
-	print(players_edgeright)
-	print(players_edgetop)
-	print(players_edgebottom)
-	print(edgeleft)
-	print(edgeright)
-	print(edgetop)
-	print(edgebottom)
-	
+#	var players_edgeleft = edgeleft + playerone_size_x
+#	var players_edgeright = edgeright - playerone_size_x
+#	var players_edgetop = edgetop + (playerone_size_y / 2)
+#	var players_edgebottom = edgebottom - (playerone_size_y / 2)
+
+		# ON START
 	playerone.position.x = edgeleft
 	playerone.position.y = edgetop
 	playertwo.position.x = edgeright
-	playertwo.position.y = edgebottom
+	playertwo.position.y = edgebottom - playertwo_size_y
+	$theball.position.x = width/2
+	$theball.position.y = height/2
 
 func _process(_delta):
-	pass
-#	frame += 1
-#	var nbcolors = colors.size()
-#	var tint = colors [which_color % nbcolors]
-#	var should_i_tint = frame % 60
-#	if should_i_tint == 0: 
-#		rect.modulate = tint
-#		which_color += 1
+
+		# PLAYERS EDGES
+		# meh...
+	if playerone.position.y >= (edgebottom - playerone_size_y):
+		playerone_cango_down = false
+	if playerone.position.y <= edgetop :
+		playerone_cango_up = false
+	if playerone.position.y < (edgebottom - playerone_size_y):
+		playerone_cango_down = true
+	if playerone.position.y > edgetop :
+		playerone_cango_up = true
 	
-#	speedy = 0
-#	speedy += -1 if Input.is_physical_key_pressed(KEY_W) else 0
-#	speedy += 1 if Input.is_physical_key_pressed(KEY_S) else 0
-#	speedx = 0
-#	speedx += -1 if Input.is_physical_key_pressed(KEY_A) else 0
-#	speedx += 1 if Input.is_physical_key_pressed(KEY_D) else 0
+	print(playerone.position.y)
 	
-#	var  = Vector2(speedx, speedy)
-#	deplacement = deplacement.normalized()
-#	$rectangle.position += deplacement * delta * SPEED
-#	$rectangle.position.x += speedx * delta
-#	$rectangle.position.y += speedy * delta
+		# VITESSE DES PLAYERS 
+		# IL FAUT SE DEBARASSER DU ELSE (TERNARY INCOMPATIBILITY)
+	if not Input.is_physical_key_pressed(KEY_W) and not Input.is_physical_key_pressed(KEY_S) :
+		speed1 = 0
+	speed1 += .5 if Input.is_physical_key_pressed(KEY_S) and speed1 <= playerSPEEDMAX else 0
+	speed1 += -.5 if Input.is_physical_key_pressed(KEY_W) and speed1 <= playerSPEEDMAX else 0
+
+
+	if not Input.is_physical_key_pressed(KEY_O) and not Input.is_physical_key_pressed(KEY_L):
+		speed2 = 0
+	if playertwo.position.y <= edgebottom and playertwo.position.y >= edgetop:
+		speed2 += -.5 if Input.is_physical_key_pressed(KEY_O) and speed2 <= playerSPEEDMAX else 0
+		speed2 += .5 if Input.is_physical_key_pressed(KEY_L) and speed2 <= playerSPEEDMAX else 0
+
+
+	playerone.position.y += speed1
+	playertwo.position.y += speed2
+
+
+		# MOUVEMENTS de LA BALLE
 	
+	var deplacement = Vector2(speedx, speedy)
+	deplacement = deplacement.normalized()
+	$theball.position += deplacement * _delta * SPEEDMAX
+#	$theball.position.x += speedx * _delta
+#	$theball.position.y += speedy * _delta
+
 func _input(ev):
-	if ev is InputEventKey:
-		if ev.physical_keycode == KEY_SPACE and ev.is_pressed() and not ev.is_echo():
-			SPEED = 450 if SPEED == 300 else 300
+	pass
 	
-#	elapsedtime = elapsedtime+delta
-#	if elapsedtime >= 1 and not coucouprinted:
-#		print("coucou")
-#		coucouprinted = true
-#		$rectangle.modulate = Color.from_hsv (randf(),1,1)
